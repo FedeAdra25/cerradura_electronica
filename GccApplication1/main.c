fb234fbefaa8d5c11ab5361de79168c384174144
FedeAdra25 <fedeadra@gmail.com>
2021-05-06 00:21:03 -0300

    first commit

---
+++ GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 06-May-21 12:19:19 AM
+ * Author : fedea
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ GccApplication1/main.h
@@ -0,0 +1,16 @@
+/*
+ * main.h
+ *
+ * Created: 06-May-21 12:19:39 AM
+ *  Author: fedea
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+
+
+#endif /* MAIN_H_ */
