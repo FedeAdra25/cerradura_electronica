2f6a0d86b075dbb1c02d343038c936688fb84e51
JeroAlfonsin <jeronimoalfonsin@gmail.com>
2021-05-07 18:41:34 -0300

    Sigo con MEF ya falta menos

--- GccApplication1/MEF.c
+++ GccApplication1/MEF.c
@@ -14,13 +14,17 @@ static uint8_t key;
 static uint8_t claveIng[4];
 static uint8_t firstTimeIdle= 1;
 static uint8_t firstTimeClaveInc= 1;
-static uint8_t firstTimeClaveCor= 1;
+static uint8_t firstTimeAbierto= 1;
+static uint8_t firstTimeMClave= 1;
 static uint8_t posClaveIng= 0;
 static uint32_t ticksPerSecond; //Se inicializa en el init
+static uint8_t terminoIng=0;
+static uint8_t firstTimeMClaveN=1;
 
 //Prototipos de funciones privadas 
 static uint8_t ClaveCorrecta(void);
 static uint8_t Clave_IncTime(void);
+static uint8_t AbiertoTime(void);
 
 
 
@@ -33,7 +37,7 @@ void MEF_update (void)
 		case IDLE :
 			if (KEYPAD_Update(&key))
 			{
-				switch key
+				switch (key)
 				{
 					case 'A':
 					break;
@@ -42,9 +46,9 @@ void MEF_update (void)
 					case 'C':
 					break;
 					case 'D':
+						ChangeM_CLAVE();
 					break;
-					default:
-						posClaveIng=0; 
+					default: 
 						changeING_CLAVE();
 					break;
 				}
@@ -54,11 +58,7 @@ void MEF_update (void)
 			else {
 					if (firstTimeIdle)
 					{
-						LCDclr();
-						//Imprimo hora
-						LCDGotoXY(4,1);
-						LCDstring("CERRADO", 7);
-						firstTimeIdle = 0;
+						OutIdle();
 					}
 					//if actualizo hora imprimir HH:MM:SS
 				 }
@@ -86,19 +86,66 @@ void MEF_update (void)
 				}
 		break;
 		case ABIERTO:
+			if(firstTimeAbierto)
+			{
+				firstTimeAbierto=0;
+				OutAbierto();
+			}
+			else if (AbiertoTime())
+			{
+				ChangeIDLE();
+			}
 		break;
 		case M_CLAVE :
+			if (firstTimeMClave || KEYPAD_Scan(&key))
+			{
+				terminoIng= OutMClave();
+			}
+			if (terminoIng)
+			{
+				if(ClaveCorrecta()) ChangeM_CLAVE_N();
+				else ChangeCLAVE_INC();
+			}
+		break;
+		case M_CLAVE_N:
+			if(firstTimeMClaveN || KEYPAD_Scan(&key))
+			{
+				terminoIng= OutMClaveN();
+			}
+			if (terminoIng)
+			{
+				if(key == 'D')
+				{
+					
+				}
+			}
 		break;
 		case M_CLAVE_E :
 		break;
 		case M_CLAVE_F :
 		break;
-		case M_CLAVE_N:
-		break;
 		case M_HORA :
 		break;
 	}
+}
 	
+	/***************************************************************
+		Funcion que sirve mostrar la salida del estado IDLE, en este
+		caso se muestra la hora y el mensaje CERRADO
+	***************************************************************/
+	void OutIdle(void)
+	{
+		if(firstTimeIdle)
+		{
+			LCDclr();
+			LCDGotoXY(4,1);
+			LCDstring("CERRADO", 7);
+			firstTimeIdle=0;
+		}
+		//LCDGotoXY(,0);
+		//Imprimo hora
+		
+	}
 	/***************************************************************
 		Funcion que sirve para transicionar del estado IDLE, al 
 		estado ING_CLAVE, guarda el caracter ingresado por el usuario,
@@ -106,6 +153,7 @@ void MEF_update (void)
 	***************************************************************/
 	void changeING_CLAVE (void)
 	{
+		state_time=0;
 		system_state= ING_CLAVE;
 		claveIng[posClaveIng]= key;
 		LCDclr();
@@ -133,12 +181,20 @@ void MEF_update (void)
 	
 	static uint8_t ClaveCorrecta(void)
 	{
-		uint8_t  i=0;
-		for(i=0; i<4; i++)
+		uint8_t lengthClaveIng= sizeof (claveIng) / sizeof(uint8_t);
+		uint8_t lengthClaveAct= sizeof (claveAct) / sizeof(uint8_t);
+		if(lengthClaveAct == lengthClaveIng)
 		{
-			if (claveIng[i] != claveAct[i]) return 0;
+			uint8_t  i=0;
+			for(i=0; i<4; i++)
+			{
+				if (claveIng[i] != claveAct[i]) return 0;
+			}
+			return 1;
+
 		}
-		return 1;
+		else return 0;
+		
 	}
 	
 	void ChangeABIERTO(void)
@@ -182,6 +238,105 @@ void MEF_update (void)
 	
 	void ChangeIDLE(void)
 	{
+		state_time=0;
+		posClaveIng=0;
 		firstTimeIdle= 1;
 		system_state= IDLE;
+	}
+	
+	
+	/***************************************************************
+		Funcion que sirve mostrar la salida del estado ABIERTO, en este
+		caso se muestra el mensaje ABIERTO
+	***************************************************************/
+	void OutAbierto(void)
+	{
+		LCDclr();
+		LCDGotoXY(4,1);
+		LCDstring("ABIERTO", 7);
+	}
+	
+	/***************************************************************
+	Funcion que determina si se cumplio el tiempo en el estado 
+	ABIERTO, en este caso chequea si el tiempo en dicho estado (en
+	ticks), es igual al tiempo tambien en ticks (por eso se multiplica
+	 el valor 3 correspondiente a los seg que se debe esta en dicho 
+	 estado por la cantidad de ticks por segundo) que se debe estar 
+	 en dicho estado
+	***************************************************************/
+	static uint8_t AbiertoTime(void)
+	{
+		if(state_time < 3*ticksPerSecond) return 0;
+		else return 1;
+	}
+	
+	void ChangeM_CLAVE(void)
+	{
+		state_time=0;
+		posClaveIng=0;
+		system_state= M_CLAVE;
+	}
+	
+	/***************************************************************
+		Funcion que sirve mostrar la salida del estado M_CLAVE, en este
+		caso se muestra el mensaje "clave actual:", y a medida que se
+		presionan las teclas *
+	***************************************************************/
+	uint8_t OutMClave(void)
+	{
+		if(firstTimeMClave)
+		{
+			LCDclr();
+			LCDstring("Clave Actual:", 13);
+			LCDGotoXY(4,1);
+			firstTimeMClave=0;
+			return 0;
+		}
+		else
+			{
+				if (key != 'D')
+				{
+					claveIng[posClaveIng]= key;
+					posClaveIng++;
+					LCDsendChar('*');
+					return 0;
+				}
+				return 1;
+			}
+	}
+	
+	void ChangeM_CLAVE_N(void)
+	{
+		state_time=0;
+		posClaveIng=0;
+		terminoIng=0;
+		system_state= M_CLAVE_N;
+	}
+	
+	/***************************************************************
+		Funcion que sirve mostrar la salida del estado M_CLAVE_N, en este
+		caso se muestra el mensaje "clave nueva:", y a medida que se
+		presionan las teclas *
+	***************************************************************/
+	uint8_t OutMClaveN(void)
+	{
+		if(firstTimeMClaveN)
+		{
+			LCDclr();
+			LCDstring("Clave nueva:", 12);
+			LCDGotoXY(4,1);
+			firstTimeMClaveN=0;
+			return 0;
+		}
+		else
+			{
+				if ((key != 'D' ) && key!= '#')
+				{
+					claveIng[posClaveIng]= key;
+					posClaveIng++;
+					LCDsendChar('*');
+					return 0;
+				}
+				return 1;
+			}
 	}
